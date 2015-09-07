fd39db9b674d209c5cd65be77eb5f5af28b1a701
Mengye Ren <renmengye@gmail.com>
2015-09-07 15:18:41 +0900

    imageqa

--- imageqa.lua
+++ imageqa.lua
@@ -2,4 +2,7 @@ local knn = require('nearest_neighbours')
 local hdf5 = require('hdf5')
 local dataPath = '/ais/gobi3/u/mren/data/cocoqa-nearest/all.h5'
 local data = hdf5.open(dataPath, 'r'):all()
-for key,value in pairs(data) do print(key,value) end
+for key,value in pairs(data) do 
+    print(key, value)
+    collectgarbage()
+end
