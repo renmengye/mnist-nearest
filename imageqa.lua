84e1e08c6f021aebf35228ebeb44ba8ec30f4f1d
Mengye Ren <renmengye@gmail.com>
2015-09-07 15:57:16 +0900

    imageqa

--- imageqa.lua
+++ imageqa.lua
@@ -9,7 +9,7 @@ local data = hdf5.open(dataPath, 'r'):all()
 -- end
 
 numTest = 100
-for k = 1,1 do
+for k = 1,21,5 do
     local validPred = knn.runAll(
         k, data.trainData, data.trainLabel, data.validData, numTest)
     local validLabelSubset = data.validLabel:index(1, torch.range(1, numTest):long())
