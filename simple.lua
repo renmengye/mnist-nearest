0a5ddaa238f4efce2c11ebc607a74b4a0d95f150
Mengye Ren <renmengye@gmail.com>
2015-09-08 13:20:51 +0900

    sync

--- simple.lua
+++ simple.lua
@@ -7,20 +7,21 @@ local knn = require('nearest_neighbours')
 ----------------------------------------------------------------------
 --local numTest = testSize[1]
 local numTest = 30
-local K = 5
-local train, test = mnist.loadData()
-local trainDataResize = mnist.flattenFloat(train.data)
-local testDataResize = mnist.flattenFloat(test.data)
-mnist.visualize(train.data)
-mnist.visualize(test.data)
-local testPred = knn.runAll(K, trainDataResize, train.labels, testDataResize, numTest)
-local testLabelsSubset = test.labels:index(1, torch.range(1, numTest):long()):long()
-local testDataSubset = test.data:index(1, torch.range(1, numTest):long())
-utils.evalPrediction(testPred, testLabelsSubset)
-local neqIdx = testPred:ne(testLabelsSubset)
-local neqIdx = neqIdx:reshape(numTest, 1, 1, 1):expand(numTest, 1, 32, 32)
-local neqExamples = testDataSubset[neqIdx]
-if neqExamples:numel() > 0 then
-    neqExamples = neqExamples:reshape(neqExamples:numel() / 1024, 1, 32, 32)
-    mnist.visualize(neqExamples)
+for K = 1,10,100 do
+    local train, test = mnist.loadData()
+    local trainDataResize = mnist.flattenFloat(train.data)
+    local testDataResize = mnist.flattenFloat(test.data)
+    mnist.visualize(train.data)
+    mnist.visualize(test.data)
+    local testPred = knn.runAll(K, trainDataResize, train.labels, testDataResize, numTest)
+    local testLabelsSubset = test.labels:index(1, torch.range(1, numTest):long()):long()
+    local testDataSubset = test.data:index(1, torch.range(1, numTest):long())
+    utils.evalPrediction(testPred, testLabelsSubset)
+    local neqIdx = testPred:ne(testLabelsSubset)
+    local neqIdx = neqIdx:reshape(numTest, 1, 1, 1):expand(numTest, 1, 32, 32)
+    local neqExamples = testDataSubset[neqIdx]
+    if neqExamples:numel() > 0 then
+        neqExamples = neqExamples:reshape(neqExamples:numel() / 1024, 1, 32, 32)
+        mnist.visualize(neqExamples)
+    end
 end
