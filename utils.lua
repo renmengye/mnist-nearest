d77575eb5ffbdf231fc847a8317abaf045d7ddbe
Mengye Ren <renmengye@gmail.com>
2015-09-08 13:17:17 +0900

    sync

--- utils.lua
+++ utils.lua
@@ -8,6 +8,9 @@ function utils.evalPrediction(prediction, labels)
     local correct = prediction:eq(labels):int():sum()
     local rate = correct / prediction:size()[1]
     logger:logInfo(string.format('Accuracy: %.5f', rate))
+    for i = 1,labels:size()[1] do
+        print labels[i]
+    end
     return rate
 end
 
