e0a3746a6a2aa3887255ca42b2e02d0c2e450a84
Mengye Ren <renmengye@gmail.com>
2015-09-14 22:35:50 -0400

    sync

---
+++ imageqa.lua
@@ -0,0 +1,23 @@
+local imageqa = {}
+function imageqa.readDict(filename)
+    fh, err = io.open(filename)
+    if err then
+        logger:logError(string.format('Error reading %s', filename))
+        return
+    end
+    while true do
+        line = fh:read()
+        if line == nil then
+            break
+        end
+        print(line)
+    end
+    fh:close()
+end
+
+-- function imageqa.decodeSentence(ids, dict)
+-- end
+
+imageqa.readDict('../data/cocoqa/answer_vocabs.txt')
+
+return imageqa
